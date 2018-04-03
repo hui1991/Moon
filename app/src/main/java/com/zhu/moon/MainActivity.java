1480de0161651032a45566a0c4c39679c8a8668c
zhuhui <13240179281@163.com>
2018-04-03 11:29:35 +0800

    test10

--- app/src/main/java/com/zhu/moon/MainActivity.java
+++ app/src/main/java/com/zhu/moon/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     private String TestCommit1;
     private String TestCommit2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
